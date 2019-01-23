d82fa1cdc55bd4135933dfd797b6ba60bacfacd9
Nico <32631608+Azruim@users.noreply.github.com>
2019-01-23 16:35:20 +0200

    Update ruutu.cpp
    pöö

--- Shakki5/ruutu.cpp
+++ Shakki5/ruutu.cpp
@@ -1,3 +1,11 @@
+//
+//  ruutu.cpp
+//  Chess
+//
+//  Created by Nico Behnen on 23/01/2019.
+//  Copyright © 2019 B & J Corp. All rights reserved.
+//
+
 #include "ruutu.h"
 
 Ruutu::Ruutu(int rivi, int sarake) {
