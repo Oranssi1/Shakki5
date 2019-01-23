ce19c41db9962e820ac1dad763da5cf452a829d3
Oranssi1 <38855324+Oranssi1@users.noreply.github.com>
2019-01-23 14:48:41 +0200

    Update kayttoliittyma.cpp

--- Shakki5/kayttoliittyma.cpp
+++ Shakki5/kayttoliittyma.cpp
@@ -3,8 +3,8 @@
 //  Chess
 //
 //  Created by Nico Behnen on 18/01/2019.
-//  Copyright © 2019 B & J Corp. All rights reserved.
-//
+//  Copyright Â© 2019 B & J Corp. All rights reserved.
+// asd
 
 #include "kayttoliittyma.h"
 #include <iostream>
