db43e81f190d354ccde48eb0a51b76d86adde821
Oranssi1 <38855324+Oranssi1@users.noreply.github.com>
2019-01-23 16:32:26 +0200

    huoh

---
+++ Shakki5/siirto.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include "siirto.h"
+
+Siirto::Siirto(Ruutu alkuruutu, Ruutu loppuruutu) {
+	_alkuRuutu = alkuruutu;
+	_loppuRuutu = loppuruutu;
+}
+
+Siirto(bool lyhytLinna, bool pitkaLinna) {
+	_lyhytLinna = lyhytLinna
+	_pitkalinna = pitkaLinna
+} // Poikkeussiirto linnoitusta varten
+
+/*
+public:
+	Siirto(Ruutu, Ruutu);
+	Siirto(bool lyhytLinna, bool pitkaLinna); // Poikkeussiirto linnoitusta varten
+	Ruutu getAlkuruutu();
+	Ruutu getLoppuruutu();
+	bool onkoLyhytLinna();
+	bool onkoPitkälinna();
+private:
+	Ruutu alkuRuutu;
+	Ruutu loppuRuutu;
+	int miksiKorotetaan = 0;
+	bool lyhytLinna;
+	bool pitkalinna;
+*/
--- Shakki5/siirto.h
+++ Shakki5/siirto.h
@@ -1,16 +1,18 @@
+#include "ruutu.h"
+
 class Siirto {
 public:
 	Siirto(Ruutu, Ruutu);
 	Siirto() {}
-	Siirto(bool lyhytLinna, bool pitkaLinna); // Poikkeussiirto linnoitusta varten
+	Siirto(bool, bool); // Poikkeussiirto linnoitusta varten
 	Ruutu getAlkuruutu();
 	Ruutu getLoppuruutu();
 	bool onkoLyhytLinna();
 	bool onkoPitkälinna();
 private:
-	Ruutu alkuRuutu;
-	Ruutu loppuRuutu;
+	Ruutu _alkuRuutu;
+	Ruutu _loppuRuutu;
 	int miksiKorotetaan = 0;
-	bool lyhytLinna;
-	bool pitkalinna;
+	bool _lyhytLinna;
+	bool _pitkalinna;
 };
