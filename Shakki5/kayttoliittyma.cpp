b4f04a23638ead8295f717ac0f1f5a879d9e4607
Oranssi1 <38855324+Oranssi1@users.noreply.github.com>
2019-02-07 14:13:45 +0200

    jotain pilkunnussintaa joka ei tarvittis syncii

--- Shakki5/kayttoliittyma.cpp
+++ Shakki5/kayttoliittyma.cpp
@@ -72,7 +72,7 @@ void Kayttoliittyma::piirraLauta() {
 
 Siirto* Kayttoliittyma::annaVastustajanSiirto() {
 	std::wstring syote;
-	bool virhe = 0;
+	bool virhe;
 	do {
 		virhe = 0;
 		std::wcout << "Anna vastustajan siirto: ";
