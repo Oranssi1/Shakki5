edab5da639aeb8d4cea800f79c450bfe8d0b2626
Oranssi1 <38855324+Oranssi1@users.noreply.github.com>
2019-02-08 11:54:09 +0200

    paskana taas

--- Shakki5/asema.cpp
+++ Shakki5/asema.cpp
@@ -18,6 +18,7 @@ Asema::Asema() {
 	}
 
 	_lauta[3][3] = new Torni(L"\u2656", 0, VT);
+	_lauta[5][2] = new Lahetti(L"\u2657", 0, VL);
 	//_lauta[0][0] = new Nappula(L"\u2656", 0, VT);
 	//_lauta[1][0] = new Nappula(L"\u2658", 0, VR);
 	//_lauta[2][0] = new Nappula(L5"\u2657", 0, VL);
