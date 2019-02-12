7a2a091174b026580e89cc44620720bdcdcd7cd3
Nico <32631608+Azruim@users.noreply.github.com>
2019-02-12 23:24:45 +0200

    korjattu

--- Shakki5/nappula.h
+++ Shakki5/nappula.h
@@ -371,39 +371,46 @@ public:
 			dy = -dy;
 		}
 		new_y = y + dy;
-		n = asema->_lauta[x][new_y];
 
-		if (n == nullptr) {
-			lista.push_back(Siirto(Ruutu(x, y), Ruutu(x, new_y)));
-			n = asema->_lauta[x-1][new_y];
-			if (n != nullptr) {
-				if (n->getVari() != vari) {
-					lista.push_back(Siirto(Ruutu(x, y), Ruutu(x-1, new_y)));
-				}
-			}
-			n = asema->_lauta[x+1][new_y];
-			if (n != nullptr) {
-				if (n->getVari() != vari) {
-					lista.push_back(Siirto(Ruutu(x, y), Ruutu(x+1, new_y)));
-				}
-			}
-		}
+		if (new_y < 8 && new_y > -1) {
+			n = asema->_lauta[x][new_y];
 
-		if (y == 1 && dy == 1 || y == 6 && dy == -1) {
-			n = asema->_lauta[x][new_y + dy];
 			if (n == nullptr) {
-				lista.push_back(Siirto(Ruutu(x, y), Ruutu(x, new_y + dy)));
-				n = asema->_lauta[x-1][new_y + dy];
-				if (n != nullptr) {
-					if (n->getVari() != vari) {
-						lista.push_back(Siirto(Ruutu(x, y), Ruutu(x-1, new_y + dy)));
+				lista.push_back(Siirto(Ruutu(x, y), Ruutu(x, new_y)));
+				if (x-1 > -1) {
+					n = asema->_lauta[x-1][new_y];
+					if (n != nullptr) {
+						if (n->getVari() != vari) {
+							lista.push_back(Siirto(Ruutu(x, y), Ruutu(x-1, new_y)));
+						}
 					}
 				}
-				n = asema->_lauta[x+1][new_y + dy];
-				if (n != nullptr) {
-					if (n->getVari() != vari) {
-						lista.push_back(Siirto(Ruutu(x, y), Ruutu(x+1, new_y + dy)));
+				if (x+1 < 8) {
+					n = asema->_lauta[x+1][new_y];
+					if (n != nullptr) {
+						if (n->getVari() != vari) {
+							lista.push_back(Siirto(Ruutu(x, y), Ruutu(x+1, new_y)));
+						}
 					}
+				}
+			}
+
+			if (y == 1 && dy == 1 || y == 6 && dy == -1 && new_y + dy < 8 && new_y + dy > -1) {
+				n = asema->_lauta[x][new_y + dy];
+				if (n == nullptr) {
+					lista.push_back(Siirto(Ruutu(x, y), Ruutu(x, new_y + dy)));
+					// n = asema->_lauta[x-1][new_y + dy];
+					// if (n != nullptr) {
+					// 	if (n->getVari() != vari) {
+					// 		lista.push_back(Siirto(Ruutu(x, y), Ruutu(x-1, new_y + dy)));
+					// 	}
+					// }
+					// n = asema->_lauta[x+1][new_y + dy];
+					// if (n != nullptr) {
+					// 	if (n->getVari() != vari) {
+					// 		lista.push_back(Siirto(Ruutu(x, y), Ruutu(x+1, new_y + dy)));
+					// 	}
+					// }
 				}
 			}
 		}
